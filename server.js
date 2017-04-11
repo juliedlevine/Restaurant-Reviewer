899f869485011b066eea87a0c40f4c48d8bc9299
juliemdyer <juliemdyer@gmail.com>
2017-04-11 13:11:10 -0400

    Added a Procfile.

--- server.js
+++ server.js
@@ -273,6 +273,6 @@ app.post('/restaurant/addReview/:id', function(req, res, next) {
 
 
 // Start server
-app.listen(3000, function() {
+app.listen(5000, function() {
     console.log('Example app listening on port 3000!');
 });
